809b17054554ad4e106469fff147f1d1cf78f008
yidreamc <yidreamc@gamil.com>
2018-10-01 20:05:23 +0800

    完成api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-import React from './react/index'
-import ReactDOM from './react-dom/index'
+import React from '../lib/react'
+import ReactDOM from '../lib/react-dom'
 
-setInterval(() => {
-    const elment = (
-        <div>
-            <h1>Hello world</h1>
-            <h2>time  {new Date().toLocaleTimeString()}</h2>
-        </div>
-    )
-    ReactDOM.render(
-        elment,
-        document.getElementById('root')
-    )
-}, 100);
+class App extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            number: 10
+        }
+    }
+    handleClick(){
+        this.setState({
+            number: this.state.number++
+        });
+    }
+    componentWillUpdate() {
+        console.log('update');
+    }
+
+    componentWillMount() {
+        console.log('mount');
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Number: { this.state.number }</h1>
+                <button onClick={() => this.handleClick()} t="1">add 1</button>
+            </div>
+        )
+    }
+}
+
+const elment = (
+    <App />
+)
+ReactDOM.render(elment,document.getElementById('root'));
--- src/react-dom/index.js
+++
@@ -1,9 +0,0 @@
-import render from './render'
-
-const ReactDOM = {
-    render: (vdom, container) => {
-        container.innerHTML = '';
-        return render(vdom, container);
-    }
-}
-export default ReactDOM;
